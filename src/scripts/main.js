c53eebbcd332493cab7e57ddee61936702b12155
Yuliia Voroniuk <juliasemchishin1545@gmail.com>
2025-11-06 15:13:13 +0200

    added solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,20 @@
 'use strict';
+
+const images = document.querySelector('.gallery__list');
+const largeImg = document.querySelector('#largeImg');
+
+// eslint-disable-next-line no-shadow
+images.addEventListener('click', (event) => {
+  const link = event.target.closest('.list-item');
+  const imagesHREf = link.querySelector('a').getAttribute('href');
+  const imgSRC = link.querySelector('img').getAttribute('src');
+
+  // console.log(imgSRC);
+
+  event.preventDefault();
+
+  largeImg.setAttribute(
+    'src',
+    window.location.origin + imagesHREf || window.location.origin + imgSRC,
+  );
+});
